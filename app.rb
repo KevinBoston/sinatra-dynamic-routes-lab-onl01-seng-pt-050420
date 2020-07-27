12c2a227c61881c15eb8bbadf8f81e6b341ef72c
Kevin Boston <kbos22@gmail.com>
2020-07-27 18:17:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,10 +11,13 @@ class App < Sinatra::Base
     output.to_s 
   end
   get '/say/:number/:phrase' do 
-    params[:phrase].
-  
+    params[:number].to_i do 
+      params[:phrase]
+    end
+  end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end
+  
 end
