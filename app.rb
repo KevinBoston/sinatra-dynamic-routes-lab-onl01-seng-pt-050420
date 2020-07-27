5309c0142bc11fc998c61c8b84acb69343601d02
Kevin Boston <kbos22@gmail.com>
2020-07-27 18:13:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  
+  get '/reversename/:name' do 
+    params[:name].reverse 
+  end
   
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
