17a67466dbfe29305ab5c397fdba68ecdf021d82
Kevin Boston <kbos22@gmail.com>
2020-07-27 18:22:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     output.to_s 
   end
   get '/say/:number/:phrase' do 
-    params[:number].to_i do 
+    params[:number].to_i.times do 
       params[:phrase]
     end
   end
@@ -19,5 +19,19 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end
+  
+  get '/:operation/:number1/:number2' do 
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    op = params[:operation]
+    if op == "add"
+      (num1+num2).to_s 
+    elsif op == "subtract"
+      (num1-num2).to_s
+    elsif op == "multiply"
+      (num1*num2).to_s 
+    elsif op == "divide"
+      (num1 / num2)
+      
   
 end
